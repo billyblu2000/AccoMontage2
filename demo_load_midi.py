4653b99e065b4f7bf876c847b08b41ac9c340ac8
billyblu2000 <ly1387@nyu.edu>
2021-07-07 00:57:33 +0800

    add midi loader

---
+++ demo_load_midi.py
@@ -0,0 +1,47 @@
+# PLEASE PUT MIDI FILES IN '/static/midi/'
+from utils.utils import MIDILoader
+
+if __name__ == '__main__':
+
+    # ##########
+    # load midis
+    # ##########
+
+    my_midis = MIDILoader()  # load every midi files in the midi folder
+    # alternatives
+    # midi = MIDILoader(files = '*') # also load every midi files in the midi folder
+    # midi = MIDILoader(files = '1.mid') # load '1.mid' in the midi folder
+    # midi = MIDILoader(files = ['1.mid','2.mid']) # load '1.mid' and '2.mid' in the midi folder
+
+    # #####################################
+    # the following code can get transformed midi data
+    # data form: [pitch, pitch, pitch, ...]
+    # each element is a sixteenth note
+    # pitch = 0 means rest
+    # #####################################
+
+    all_midis = my_midis.all()
+
+    data_of_a_specific_midi = my_midis.get(name='6.mid')
+    # alternatives
+    # data_of_a_specific_midi = my_midis.get(name=['3.mid','4.mid']) # pick 3.mid and 4.mid
+
+    pick_midis_randomly = my_midis.sample()
+    # alternatives
+    # pick_midis_randomly = my_midis.sample(num=3) # pick 3 midis
+
+    print("All MIDIs: ", all_midis)
+    print("Data of '3.mid': ", data_of_a_specific_midi)
+    print("Pick a MIDI randomly: ", pick_midis_randomly)
+
+    # switch output form to 1234567
+    my_midis.config(output_form='number')
+
+    number_data_of_a_specific_midi = my_midis.get(name='6.mid')
+    print("Make the melody read-friendly ('6.mid' for example)': ", number_data_of_a_specific_midi)
+
+    # switch output form to midi object
+    my_midis.config(output_form='midi')
+
+    midi_object_of_a_specific_midi = my_midis.get(name='6.mid')
+    print("Return a MIDI object': ", midi_object_of_a_specific_midi)
--- utils/utils.py
+++ utils/utils.py
@@ -1,3 +1,4 @@
+import random
 import time
 
 from pretty_midi import *
@@ -280,6 +281,130 @@ def pick_progressions(*args, **kwargs):
                 continue
         new_list.append(i)
     return new_list
+
+
+class MIDILoader:
+
+    def __init__(self, midi_dir=STATIC_DIR + 'midi/', files="*"):
+        self.midi_dir = midi_dir
+        self.midis = []
+        self.transformed = []
+        self.roll = []
+        self._config = {
+            'output_form': 'pitch'
+        }
+        self.load_midis(files)
+        self.process()
+
+    def config(self, output_form='pitch'):
+        if output_form not in ['pitch', 'number', 'midi']:
+            raise ValueError
+        self._config['output_form'] = output_form
+
+    def __get_data(self):
+        if self._config['output_form'] == 'midi':
+            return self.midis
+        elif self._config['output_form'] == 'pitch':
+            return self.transformed
+        else:
+            return self.roll
+
+    def load_midis(self, files):
+        try:
+            if files == '*':
+                files = os.listdir(self.midi_dir)
+                for file in files:
+                    if file[-4:] == '.mid':
+                        midi = PrettyMIDI(os.path.join(self.midi_dir, file))
+                        tonic = 'C'
+                        self.midis.append((file, tonic, midi))
+            elif type(files) is list:
+                for file_name in files:
+                    if file_name[-4:] == '.mid':
+                        midi = PrettyMIDI(os.path.join(self.midi_dir, file_name))
+                        tonic = 'C'
+                        self.midis.append((file_name, tonic, midi))
+            elif type(files) is str:
+                midi = PrettyMIDI(os.path.join(self.midi_dir, files))
+                tonic = 'C'
+                self.midis.append((files, tonic, midi))
+            else:
+                raise ValueError("argument 'files' must be '*' or file name or list of file names")
+        except Exception as e:
+            raise Exception('An error occored when loading midis')
+
+    def process(self):
+        new = []
+        for midi in self.midis:
+            ins = midi[2].instruments[0]
+            tempo = midi[2].get_tempo_changes()[1][0]
+            note_list = np.zeros(10240, dtype=int)
+            very_end = 0
+            for note in ins.notes:
+                start, end = self._get_note_location_from_start_end_time(note.start, note.end, tempo)
+                if end > very_end:
+                    very_end = end
+                for i in range(start, end):
+                    note_list[i] = int(note.pitch)
+            note_list = note_list[:very_end]
+            note_list = list(note_list)
+            new.append((midi[0], midi[1], note_list))
+        self.transformed = new
+
+        new = []
+        major_map = {
+            0: 1, 1: 1.5, 2: 2, 3: 2.5, 4: 3, 5: 4, 6: 4.5, 7: 5, 8: 5.5, 9: 6, 10: 6.5, 11: 7
+        }
+        minor_map = {
+            0: 1, 1: 1.5, 2: 2, 3: 3, 4: 3.5, 5: 4, 6: 4.5, 7: 5, 8: 6, 9: 6.5, 10: 7, 11: 7.5
+        }
+        for midi in self.transformed:
+            tonic_index = str_to_root[midi[1]]
+            note_list = []
+            map = major_map
+            for pitch in midi[2]:
+                if pitch == 0:
+                    note_list.append(0)
+                else:
+                    note_list.append(map[(pitch - tonic_index) % 12])
+            new.append((midi[0],midi[1],note_list))
+        self.roll = new
+
+    @staticmethod
+    def _get_note_location_from_start_end_time(start_time, end_time, tempo):
+        unit_length = 15 / tempo
+        start_loc = int(start_time / unit_length)
+        end_loc = int(end_time // unit_length + 2)
+        return start_loc, end_loc
+
+    def all(self):
+        return self.sample(num=len(self.midis))
+
+    def get(self, name='*'):
+        if name == "*":
+            return self.sample()
+        elif type(name) is list:
+            midis = self.__get_data()
+            picked_midis = []
+            for i in range(len(midis)):
+                if midis[i][0] in name:
+                    picked_midis.append(midis[i][2])
+            return picked_midis
+        elif type(name) is str:
+            midis = self.__get_data()
+            for i in range(len(midis)):
+                if midis[i][0] == name:
+                    return midis[i][2]
+        else:
+            raise ValueError("name")
+
+    def sample(self, num=1):
+        midis = self.__get_data()
+        sampled = random.sample(midis, k=num)
+        sampled = [i[2] for i in sampled]
+        if num == 1:
+            sampled = sampled[0]
+        return sampled
 
 
 if __name__ == '__main__':
