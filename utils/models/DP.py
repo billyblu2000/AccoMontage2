68d50cbcfbce558bc94ab9ebd66c5a69b4267460
billyyi <ly1387@nyu.edu>
2021-07-20 18:53:40 +0800

    load midi update

---
+++ utils/models/DP.py
@@ -0,0 +1,10 @@
+class DP:
+
+    def __init__(self,templates, melo_meta, melo):
+        self.templates = templates
+        self.melo_meta = melo_meta
+        self.melo = melo
+        pass
+
+    def solve(self):
+        pass
