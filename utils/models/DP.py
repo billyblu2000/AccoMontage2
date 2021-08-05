078a62417ea6e1d61e5229d642c01ef773d646ec
billyyi <ly1387@nyu.edu>
2021-08-05 19:27:35 +0800

    update dp

--- utils/models/DP.py
+++ utils/models/DP.py
@@ -7,45 +7,48 @@ from typing import List
 
 # core DP algorithm
 class DP:
+    _dp: List[List[float, List[Chord]]]
+    result: List[Chord]
 
     def __init__(self, melo: list, melo_meta: dict, templates: List[ChordProgression]):
         self.melo = melo
         self.melo_meta = melo_meta
         self.templates = templates
         self.template_span = self.__span_template()
+        self.max_template_length = max([len(i) for i in self.template_span])
         self.solved = False
+        self._dp = [[0.0, []]]
         self.result = []
+
         self.solve()
 
     def solve(self):
-        pass
+        all_chords = self.__get_all_available_chords()
+        cursor = 2
+        while cursor <= len(self.melo):
+            melody_seg = self.melo[:cursor]
+            candidate_pool = []
+            for chord in all_chords:
+                exploration = self._dp[(cursor - 2) // 2][1] + [chord]
+                candidate_pool.append(exploration)
+            for i in range(len(self._dp)):
+
+
         self.solved = True
+        pass
+
+    def __get_all_available_chords(self) -> List[Chord]:
         pass
 
     def __span_template(self) -> List[List[List[Chord], float]]:
         pass
 
-    def __match_template(self, progression: list) -> int:
+    def __match_progression_and_template_span(self, progression: list) -> float:
         pass
 
-    def __bar_melody_to_chord_score(self, bar_melody: list) -> dict:
-        if bar_melody == [1]:
-            chord_score = {1: 1, 2: 0.5, 3: 0.5, 4: 0.9, 5: 0.5, 6: 0.8, 7: 0.5}
-        elif bar_melody == [2]:
-            chord_score = {1: 0.5, 2: 1, 3: 0.5, 4: 0.5, 5: 0.9, 6: 0.5, 7: 0.8}
-        elif bar_melody == [3]:
-            chord_score = {1: 0.8, 2: 0.5, 3: 1, 4: 0.5, 5: 0.5, 6: 0.9, 7: 0.5}
-        elif bar_melody == [4]:
-            chord_score = {1: 0.5, 2: 0.8, 3: 0.5, 4: 1, 5: 0.5, 6: 0.5, 7: 0.9}
-        elif bar_melody == [5]:
-            chord_score = {1: 0.9, 2: 0.5, 3: 0.8, 4: 0.5, 5: 1, 6: 0.5, 7: 0.5}
-        elif bar_melody == [6]:
-            chord_score = {1: 0.5, 2: 0.9, 3: 0.5, 4: 0.8, 5: 0.5, 6: 1, 7: 0.5}
-        elif bar_melody == [7]:
-            chord_score = {1: 0.5, 2: 0.5, 3: 0.9, 4: 0.5, 5: 0.8, 6: 0.5, 7: 1}
-        else:
-            chord_score = {1: 1, 2: 1, 3: 1, 4: 1, 5: 1, 6: 1, 7: 1}
-        return chord_score
+    @staticmethod
+    def __match_melody_and_chord(melody_list: list, chord_list: list) -> float:
+        pass
 
     def get_progression(self) -> ChordProgression:
         if self.solved:
