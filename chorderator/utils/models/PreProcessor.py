6b9c4be061253ff15eab0403058ce894e44e7738
billyblu2000 <ly1387@nyu.edu>
2021-09-18 17:26:46 +0800

    update

--- chorderator/utils/models/PreProcessor.py
+++ chorderator/utils/models/PreProcessor.py
@@ -5,16 +5,23 @@ from utils.utils import MIDILoader
 
 class PreProcessor:
 
+    use_test = True
+
     def __init__(self, midi_path, meta):
-        self.midi_path = midi_path
+        self.midi = PrettyMIDI(midi_path)
         self.meta = meta
 
     def get(self):
         # TODO temp for test!
-        pop909_loader = MIDILoader(files='POP909')
-        pop909_loader.config(output_form='number')
-        melo_source_name = MIDILoader.auto_find_pop909_source_name(start_with='114')[:5]
-        test_melo = [pop909_loader.get(name=i) for i in melo_source_name]
-        self.meta['pos'] = [name[6] for name in melo_source_name]
+        if PreProcessor.use_test:
+            pop909_loader = MIDILoader(files='POP909')
+            pop909_loader.config(output_form='number')
+            melo_source_name = MIDILoader.auto_find_pop909_source_name(start_with='114')[:5]
+            test_melo = [pop909_loader.get(name=i) for i in melo_source_name]
+            self.meta['pos'] = [name[6] for name in melo_source_name]
+
+        else:
+            test_melo = []
+            self.meta['pos'] = []
 
         return test_melo, self.meta
