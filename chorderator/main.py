37b5107b9d0a9740c0fa4264c8665aacaeb42f3a
billyblu2000 <ly1387@nyu.edu>
2021-09-18 23:09:05 +0800

    update pickle cache

--- chorderator/main.py
+++ chorderator/main.py
@@ -3,6 +3,7 @@
 # #######
 import pickle
 
+import pandas
 from pretty_midi import PrettyMIDI, Instrument, Note
 from matplotlib import pyplot as plt
 
@@ -14,38 +15,49 @@ from utils.string import STATIC_DIR
 
 from utils.utils import MIDILoader, listen, Logging, pick_progressions, np, calculate_density, read_lib
 
-my_progressions = read_progressions()
-my_dict = {}
-count_key = 0
-count = 0
-for i in my_progressions:
-    count += 1
-    print(count)
-    for key in my_dict.keys():
-        if my_dict[key][0] == i:
-            my_dict[key].append(i)
-            break
-    else:
-        count_key += 1
-        print(count_key)
-        my_dict[count_key] = [i]
-progressions_representative = [lst[0] for lst in my_dict.values()]
-for item in my_dict.items():
-    new = []
-    for p in item[1]:
-        p.progression_class['duplicate-id'] = item[0]
-        new.append(p)
-    my_dict[item[0]] = new
+# my_progressions = read_progressions()
+# my_dict = {}
+# count_key = 0
+# count = 0
+# for i in my_progressions:
+#     count += 1
+#     print(count)
+#     for key in my_dict.keys():
+#         if my_dict[key][0] == i:
+#             my_dict[key].append(i)
+#             break
+#     else:
+#         count_key += 1
+#         print(count_key)
+#         my_dict[count_key] = [i]
+# progressions_representative = [lst[0] for lst in my_dict.values()]
+# for item in my_dict.items():
+#     new = []
+#     for p in item[1]:
+#         p.progression_class['duplicate-id'] = item[0]
+#         new.append(p)
+#     my_dict[item[0]] = new
+#
+#
+# file = open('progressions_dict.pcls', 'wb')
+# pickle.dump(my_dict, file)
+# file.close()
+#
+# file = open('progressions_representative.pcls', 'wb')
+# pickle.dump(progressions_representative, file)
+# file.close()
 
-
-file = open('progressions_dict.pcls', 'wb')
-pickle.dump(my_dict, file)
-file.close()
-
-file = open('progressions_representative.pcls', 'wb')
-pickle.dump(progressions_representative, file)
-file.close()
-
-# p = read_progressions('progressions_representative.pcls')
-# print_progression_list(p)
+p = read_progressions('progressions_dict.pcls')
+lst = []
+for i in p.items():
+    sub_lst = [i[0], len(i[1])]
+    progression = i[1][0].get(only_root=True, flattened=True)
+    sequence = [progression[0]]
+    for j in progression:
+        sequence.append(j) if j != sequence[-1] else None
+    sub_lst.append(sequence)
+    sub_lst.append(i[1][0].meta['source'])
+    sub_lst.append(i[1][-1].meta['source'])
+    lst.append(sub_lst)
+pandas.DataFrame(lst).to_excel('out.xlsx',index=False)
 
