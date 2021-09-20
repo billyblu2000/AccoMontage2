2d2715579627f0fc51cd4676b5b30f16c734bc1a
billyblu2000 <ly1387@nyu.edu>
2021-09-20 19:32:18 +0800

    update

--- chorderator/utils/models/PostProcessor.py
+++ chorderator/utils/models/PostProcessor.py
@@ -4,29 +4,30 @@ import random
 class PostProcessor:
     def __init__(self, progression_list, progression_lib):
         self.progression_list = progression_list
-        self.progression_lib = self.create_dup_progression_list(progression_lib)
-        self.evaluate_reliability()
+        self.progression_lib = self.__create_dup_progression_list(progression_lib)
+        self.progression_lib_filtered = self.__evaluate_reliability(self.progression_lib)
 
     def get(self):
         final_out = []
-        for i in range(len(self.progression_lib)):
-            if len(self.progression_lib[i]) == 0:
-                final_out.append(self.progression_list[i])
+        for i in range(len(self.progression_lib_filtered)):
+            if len(self.progression_lib_filtered[i]) == 0:
+                final_out.append(max(self.progression_lib[i], key=lambda x: x.reliability))
             else:
-                final_out.append(random.choice(self.progression_lib[i]))
+                final_out.append(random.choice(self.progression_lib_filtered[i]))
         return final_out
 
-    def evaluate_reliability(self, threshold=0.8):
+    @staticmethod
+    def __evaluate_reliability(progression_lib, threshold=0.8):
         new_list = []
-        for lst in self.progression_lib:
+        for lst in progression_lib:
             new_sub_list = []
             for progression in lst:
                 if progression.reliability >= threshold:
                     new_sub_list.append(progression)
             new_list.append(new_sub_list)
-        self.progression_lib = new_list
+        return new_list
 
-    def create_dup_progression_list(self, progression_lib):
+    def __create_dup_progression_list(self, progression_lib):
         new_list = []
         for progression in self.progression_list:
             dup_id = progression.progression_class['duplicate-id']
