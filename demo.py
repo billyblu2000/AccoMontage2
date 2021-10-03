9095e4b7dd475e07adfad7c4ce00aed14bef59cf
billyblu2000 <ly1387@nyu.edu>
2021-10-04 01:35:57 +0800

    span redistributed

--- demo.py
+++ demo.py
@@ -3,12 +3,8 @@ import pickle
 import chorderator as cdt
 from chorderator import Const
 from chords.ChordProgression import read_progressions
-from matplotlib import pyplot as plt
 
-from utils.utils import read_lib, split_huge_progression_dict
+from utils.utils import read_lib, split_huge_progression_dict, PathGenerator
 
 if __name__ == '__main__':
-    prog = read_progressions('span_progressions_dict.pcls')
-    print(sum(len(i) for i in prog.values()))
-
-
+    pass
