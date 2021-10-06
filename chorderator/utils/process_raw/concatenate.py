c18e3a4e492090d16f31072b90af4259aa9888f2
billyblu2000 <ly1387@nyu.edu>
2021-10-07 01:01:28 +0800

    update concatenate.py

--- chorderator/utils/process_raw/concatenate.py
+++ chorderator/utils/process_raw/concatenate.py
@@ -1,152 +1,73 @@
+import pickle
 from typing import List
-from chords.Chord import Chord
-from chords.ChordProgression import ChordProgression, read_progressions, print_progression_list
+from chords.ChordProgression import ChordProgression, read_progressions
+from utils.string import STATIC_DIR
 
 
 class Concatenate:
-    def __init__(self, aim_length: [4, 8, 12, 16, 24, 32], templates: List[ChordProgression], transition_score: dict):
+    concat_rule = {
+        4: [[4]],
+        8: [[4, 4], [8]],
+        12: [[4, 4, 4], [4, 8], [8, 4]],
+        16: [[8, 8], [16]],
+        24: [[8, 8, 8], [16, 8], [8, 16]],
+        32: [[16, 16]]
+    }
+
+    def __init__(self, templates: List[ChordProgression], transition_score: dict):
         self.templates = templates
         self.threshold = 0  # 每个接合允许的最低分数
-        self.aim_length = aim_length
         self.transition_score = transition_score
         self.max_score = 1  # 对于不拼接的 打满分
 
-    # def select_candidates(self):
-    #     templates_len4 = []
-    #     templates_len8 = []
-    #     templates_len12 = []
-    #     templates_len16 = []
-    #     templates_len24 = []
-    #     templates_len32 = []
-    #     for template in self.templates:
-    #         if len(template) == 4:
-    #             templates_len4.append(template)
-    #         if len(template) == 8:
-    #             templates_len8.append(template)
-    #         if len(template) == 12:
-    #             templates_len12.append(template)
-    #         if len(template) == 16:
-    #             templates_len16.append(template)
-    #         if len(template) == 24:
-    #             templates_len24.append(template)
-    #         if len(template) == 32:
-    #             templates_len32.append(template)
-    #
+    def compute_transition(self, prog_list):
+        if len(prog_list) == 1:
+            return self.max_score
+        else:
+            min_score, cursor = 1, 1
+            while cursor < len(prog_list):
+                transition_bars = prog_list[cursor - 1].progression[-1] + prog_list[cursor].progression[0]
+                score = self.transition_score[tuple(transition_bars)]
+                if score < min_score:
+                    min_score = score
+                cursor += 1
+            return min_score
+
+    def combinations(self, ingredients_lists):
+        if type(ingredients_lists[0][0]) is not list:
+            ingredients_lists[0] = [[prog] for prog in ingredients_lists[0]]
+        if len(ingredients_lists) == 1:
+            return ingredients_lists[0]
+        if len(ingredients_lists[1]) == 0:
+            return ingredients_lists[0]
+        else:
+            recur_memo = []
+            for comb in ingredients_lists[0]:
+                for item in ingredients_lists[1]:
+                    recur_memo.append(comb + [item])
+            return self.combinations([recur_memo]+ ingredients_lists[2:])
 
     def concatenate(self):
         available_templates = []
 
-        templates_len4 = []
-        templates_len8 = []
-        templates_len12 = []
-        templates_len16 = []
-        templates_len24 = []
-        templates_len32 = []
+        ingredients_length = set(k for i in self.concat_rule.values() for j in i for k in j)
+        ingredients = {length: [] for length in ingredients_length}
         for template in self.templates:
-            if len(template) == 4:
-                templates_len4.append(template)
-            if len(template) == 8:
-                templates_len8.append(template)
-            if len(template) == 12:
-                templates_len12.append(template)
-            if len(template) == 16:
-                templates_len16.append(template)
-            if len(template) == 24:
-                templates_len24.append(template)
-            if len(template) == 32:
-                templates_len32.append(template)
-
-
-        if self.aim_length == 4:
-            available_templates = templates_len4
-
-        if self.aim_length == 8:
-            candidates44 = []
-            for i in templates_len4:
-                for j in templates_len4:
-                    if self.transition_score(i, j) > self.threshold:
-                        candidates44.append((self.transition_score(i, j), i.progression + j.progression))
-
-            for i in templates_len8:
-                available_templates.append((self.max_score, i.progression))
-            available_templates += candidates44
-
-        if self.aim_length == 12:
-            candidates444 = []
-            for i in templates_len4:
-                for j in templates_len4:
-                    for k in  templates_len4:
-                        score = min(self.transition_score(i, j), self.transition_score(j, k))
-                    if score > self.threshold:
-                        candidates444.append((score, i.progression + j.progression + k.progression))
-            available_templates += candidates444
-
-            candidates48 = []
-            for i in templates_len4:
-                for j in templates_len8:
-                    if self.transition_score(i, j) > self.threshold:
-                        candidates48.append((self.transition_score(i, j), i.progression + j.progression))
-            available_templates += candidates48
-
-            candidates84 = []
-            for i in templates_len8:
-                for j in templates_len4:
-                    if self.transition_score(i, j) > self.threshold:
-                        candidates84.append((self.transition_score(i, j), i.progression + j.progression))
-            available_templates += candidates84
-
-            for i in templates_len12:
-                available_templates.append((self.max_score, i.progression))
-
-        if self.aim_length == 16:
-            candidates88 = []
-            for i in templates_len8:
-                for j in templates_len8:
-                    if self.transition_score(i, j) > self.threshold:
-                        candidates88.append((self.transition_score(i, j), i.progression + j.progression))
-
-            for i in templates_len16:
-                available_templates.append((self.max_score, i.progression))
-            available_templates += candidates88
-
-        if self.aim_length == 24:
-            candidates888 = []
-            for i in templates_len8:
-                for j in templates_len8:
-                    for k in templates_len8:
-                        score = min(self.transition_score(i, j), self.transition_score(j, k))
-                    if score > self.threshold:
-                        candidates888.append((score, i.progression + j.progression + k.progression))
-            available_templates += candidates888
-
-            candidates816 = []
-            for i in templates_len8:
-                for j in templates_len16:
-                    if self.transition_score(i, j) > self.threshold:
-                        candidates816.append((self.transition_score(i, j), i.progression + j.progression))
-            available_templates += candidates816
-
-            candidates168 = []
-            for i in templates_len16:
-                for j in templates_len8:
-                    if self.transition_score(i, j) > self.threshold:
-                        candidates168.append((self.transition_score(i, j), i.progression + j.progression))
-            available_templates += candidates168
-
-            for i in templates_len24:
-                available_templates.append((self.max_score, i.progression))
-
-        if self.aim_length == 32:
-            candidates1616 = []
-            for i in templates_len16:
-                for j in templates_len16:
-                    if self.transition_score(i, j) > self.threshold:
-                        candidates1616.append((self.transition_score(i, j), i.progression + j.progression))
-            available_templates += candidates1616
-
-            for i in templates_len32:
-                available_templates.append((self.max_score, i.progression))
-
+            if len(template)//8 in ingredients_length:
+                ingredients[len(template)//8].append(template)
+        for aim_length in self.concat_rule:
+            for rule in self.concat_rule[aim_length]:
+                print(rule)
+                for combination in self.combinations([ingredients[length] for length in rule]):
+                    score = self.compute_transition(combination)
+                    if score >= self.threshold:
+                        available_templates.append((score, [prog.progression_class['duplicate-id']
+                                                            for prog in combination]))
 
         return available_templates
 
+
+if __name__ == '__main__':
+    my_concatenater = Concatenate(templates=read_progressions('representative.pcls'),
+                                  transition_score=pickle.load(open(STATIC_DIR + 'transition_score.mdch', 'rb')))
+    all = my_concatenater.concatenate()
