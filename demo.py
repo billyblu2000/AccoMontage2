a5359178f3fa4929697e07a467c6752ccdd43832
billyblu2000 <ly1387@nyu.edu>
2021-11-02 23:41:18 +0800

    update input midi

--- demo.py
+++ demo.py
@@ -1,21 +1,7 @@
-import math
-import pickle
-from matplotlib import pyplot as plt
-
-import numpy
-from matplotlib import pyplot
-
 import chorderator as cdt
-from chords.ChordProgression import read_progressions, print_progression_list
-from utils.string import STATIC_DIR
 
-from utils.utils import read_lib, split_huge_progression_dict, PathGenerator, MIDILoader
 
-# cdt.set_preprocess_model('PreProcessor')
-# cdt.set_main_model('DP')
-# cdt.set_postprocess_model('PostProcessor')
-
-cdt.set_melody('MIDI demos/inputs/3.mid')
+cdt.set_melody('MIDI demos/inputs/4.mid')
 cdt.set_phrase([1])
 cdt.set_meta(tonic=cdt.Key.C, mode=cdt.Mode.MAJOR, meter=cdt.Meter.FOUR_FOUR)
 
