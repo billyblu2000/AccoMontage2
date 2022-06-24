15aafe577baf23191d02cef801891c9ea9884cb2
Billy Yi <ly1387@nyu.edu>
2022-06-24 22:16:55 +0400

    test on complex chord

---
+++ complex_chord_test.py
@@ -0,0 +1,13 @@
+import chorderator as cdt
+
+if __name__ == '__main__':
+
+    input_melody_path = 'complex_chord_test/sample.mid'
+
+    cdt.set_melody(input_melody_path)
+    cdt.set_meta(tonic=cdt.Key.G)
+    cdt.set_segmentation('A8')
+    cdt.set_texture_prefilter((1, 1))
+    cdt.set_note_shift(0)
+    cdt.set_output_style(cdt.Style.POP_STANDARD)
+    cdt.generate_save('complex_chord_test', task='texture', cut_in_arg=input_melody_path, log=False)
