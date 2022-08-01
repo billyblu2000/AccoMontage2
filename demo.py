471a72d9232a1097d4f9a68649cc22e46f9bdb5f
Billy Yi <ly1387@nyu.edu>
2022-08-01 15:04:43 +0400

    update demo

--- demo.py
+++ demo.py
@@ -1,15 +1,14 @@
 import chorderator as cdt
-import json
 
 if __name__ == '__main__':
 
-    demo_name = 'hpps65'
+    demo_name = 'hpps30'
     input_melody_path = 'MIDI demos/inputs/' + demo_name + '/melody.mid'
 
     cdt.set_melody(input_melody_path)
-    cdt.set_meta(tonic=cdt.Key.G)
-    cdt.set_segmentation('A8A8B8B8')
-    cdt.set_texture_prefilter((4, 4))
-    cdt.set_note_shift(0)
-    cdt.set_output_style(cdt.Style.POP_COMPLEX)
-    cdt.generate_save(demo_name)
+    cdt.set_meta(tonic=cdt.Key.A)
+    cdt.set_segmentation('A8B8A8B8')
+    cdt.set_texture_prefilter((0, 2))
+    cdt.set_note_shift(16)
+    cdt.set_output_style(cdt.Style.POP_STANDARD)
+    cdt.generate_save(demo_name + '_output_results')
